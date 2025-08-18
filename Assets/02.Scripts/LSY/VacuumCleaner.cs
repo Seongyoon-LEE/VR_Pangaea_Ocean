67113315dc3cce6fa3572d04d7a300acb544ec5c
Seongyoon-LEE <ssyy9727@naver.com>
2025-08-18 17:58:05 +0900

    feat: 청소기 흡입 스크립트 구현중

--- Assets/02.Scripts/LSY/VacuumCleaner.cs
+++ Assets/02.Scripts/LSY/VacuumCleaner.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class VacuumCleaner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] ParticleSystem inhalationPS;
+    [SerializeField] LayerMask layerMask; // 레이저가 충돌할 레이어를 지정합니다.
+    [SerializeField] Transform shootSource; // 레이저가 발사되는 위치
+    [SerializeField] float rayDistance = 10f; // 레이저의 최대 거리
+    bool rayActivate  = false;
     void Start()
     {
+      XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.activated.AddListener((args) =>
+        {
+            if (inhalationPS != null)
+                inhalationPS.Play();
+            rayActivate = true; // 레이저 활성화
+        });
+
+        grabInteractable.deactivated.AddListener(x => StopShoot());
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (rayActivate)
+            RaycastCheck();
+    }
+    public void StopShoot()
+    {
+        // 광선 쏘고 나서 바로 파티클 잔여물 사라지게
+        inhalationPS.Stop(true,ParticleSystemStopBehavior.StopEmittingAndClear);
+        rayActivate = false; // 레이저 비활성화
+    }
+    void RaycastCheck()
+    {
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(shootSource.position, shootSource.forward, out hit, rayDistance, layerMask);
+        if (hasHit)
+        {
+            hit.collider.gameObject.SendMessage("Break", SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
