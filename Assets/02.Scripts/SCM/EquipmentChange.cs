b61d69e3fa496ef007037ede2b583327e3958bcb
schang94 <changmin947@gmail.com>
2025-08-19 11:24:00 +0900

    feat: 장비변경

--- Assets/02.Scripts/SCM/EquipmentChange.cs
+++ Assets/02.Scripts/SCM/EquipmentChange.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 public class EquipmentChange : MonoBehaviour
 {
-    public XRBaseInteractable[] equipmentList;
-    public Image[] equipmentImage;
+    public GameObject[] equipmentList;
     public InputActionProperty inputChange;
-    private int changeNum = 0;
-    private Canvas canvas;
+    private GameObject canvas;
+    private Transform head;
+    private GameObject ray;
+    private float spawnDistance = 2f;
+    private int selectNum = 0;
     void Start()
     {
-        canvas = GameObject.Find("Canvas_ChangUI").GetComponent<Canvas>(); ;
-        canvas.gameObject.SetActive(false);
-
+        canvas = GameObject.Find("Canvas_ChangUI").GetComponent<Canvas>().gameObject;
+        canvas.SetActive(false);
+        head = GameObject.Find("XR Origin (XR Rig)").transform.GetChild(0).transform;
+        // 자식수 보고 세팅하기
+        ray = GameObject.Find("XR Origin (XR Rig)").transform.GetChild(0).GetChild(3).gameObject;
+        ray.SetActive(false);
     }
 
 
@@ -23,18 +29,36 @@ public class EquipmentChange : MonoBehaviour
     {
         if (inputChange.action.WasPressedThisFrame())
         {
-            StartCoroutine(ChangeUI());
-            equipmentList[changeNum++].gameObject.SetActive(false);
-            if (changeNum == equipmentList.Length) changeNum = 0;
-            equipmentList[changeNum].gameObject.SetActive(true);
+            UIEnable();
+            canvas.transform.position = head.position + new Vector3(head.forward.x, 0f, head.forward.z).normalized * spawnDistance;
+
+            for (int i = 0; i < equipmentList.Length - 1; i++)
+            {
+                if (equipmentList[i].activeSelf)
+                    selectNum = i;
+            }
         }
     }
 
-
-    IEnumerator ChangeUI()
+    public void EquipmentSelect(int select)
     {
-        canvas.gameObject.SetActive(true);
-        yield return new WaitForSeconds(1f);
-        canvas.gameObject.SetActive(false);
+        for (int i = 0; i < equipmentList.Length - 1; i++)
+        {
+            if (i == select) equipmentList[i].SetActive(true);
+            else equipmentList[i].SetActive(false);
+
+            equipmentList[equipmentList.Length - 1].SetActive(equipmentList[0].activeSelf);
+        }
+    }
+
+    public void CloseBtn()
+    {
+        UIEnable();
+        EquipmentSelect(selectNum);
+    }
+    private void UIEnable()
+    {
+        canvas.SetActive(!canvas.activeSelf);
+        ray.SetActive(!ray.activeSelf);
     }
 }
