19ffc810ed5e1d22d27e127a8e1230bf1641d8fb
BUSWINDOW <rldudtj99@gmail.com>
2025-08-19 14:18:06 +0900

    0819_ 심리스 구조, 쓰래기 데이터 구현

---
+++ Assets/02.Scripts/KYS/Seamless/TrashInfo.cs
@@ -0,0 +1,21 @@
+[System.Serializable]
+public class TrashInfo
+{
+    public int id;
+    //public Vector2Int cell;
+    public int cellX;
+    public int cellY;
+    public int kind;
+    public int status;
+    //public Vector3 position;
+    public float posX;
+    public float posY;
+    public float posZ;
+
+    //public Quaternion rotation;
+    public float rotX;
+    public float rotY;
+    public float rotZ;
+
+    public float weight;
+}
