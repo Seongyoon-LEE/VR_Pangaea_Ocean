405aba609240a0295a0886a44a561cadf403a797
BUSWINDOW <rldudtj99@gmail.com>
2025-08-22 16:32:32 +0900

    feat: 큰 쓰레기, 해초 쓰레기 구현

---
+++ Assets/02.Scripts/KYS/Trash/TrashData.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class TrashData : MonoBehaviour
+{
+    [SerializeField] // 테스트 끝나면 지워도됨
+    private TrashInfo _info;
+    public TrashInfo Info
+    {
+        get
+        {
+            return this._info;
+        }
+        set
+        {
+            this._info = value;
+
+            this.Init(); // 종류에 따라 다른 초기화
+        }
+    }
+    public void Clean()
+    {
+        this.Info.status = (int)TrashStatus.Clean; // 쓰레기 상태를 청소로 변경
+    }
+    public virtual void Init()
+    {
+        this.transform.position = new Vector3(this.Info.posX, this.Info.posY, this.Info.posZ);
+        this.transform.rotation = Quaternion.Euler(this.Info.rotX, this.Info.rotY, this.Info.rotZ);
+        this.transform.Translate(0, this.Info.height, 0,Space.World); // 높이값을 적용
+    }
+    public virtual void DisActivate()
+    {
+        this.gameObject.SetActive(false); // 쓰레기 오브젝트 비활성화
+    }
+}
