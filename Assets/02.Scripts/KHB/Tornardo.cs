28d99cb5bfebc7848460162b0fc1c9f1e508ee6e
kshhsk156 <kshhsk156@gmail.com>
2025-08-26 12:45:06 +0900

    feat : 토네이도 애셋 및 일정 범위 빨아들임 구현
    토네이도 범위(기즈모) 내에 있을 시 플레이어만 빨아들이게끔 구현

---
+++ Assets/02.Scripts/KHB/Tornardo.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tornardo : MonoBehaviour
+{
+    public float RotateSpeed = 300f;
+
+    public float pullRadius = 500f; // 회오리가 물체를 끌어당기는 반경
+
+    private float currentPullForce = 300f; // 매니저 스크립트로부터 전달받는 현재 당기는 힘
+
+
+    public void SetPullForce(float force)
+    {
+        currentPullForce = Mathf.Abs(force);
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward * RotateSpeed * Time.deltaTime);
+        // 당기는 힘이 없으면 아무것도 안함.
+        if (currentPullForce <= 0) return;
+
+        // 회오리 주변의 모든 콜라이더(Collider)를 찾음.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, pullRadius);
+
+
+
+        foreach (Collider hit in colliders)
+        {
+            
+                //회오리 오브젝트 자신은 힘을 받지 않도록 스킵
+                if (hit.transform == transform) continue;
+
+            if (hit.CompareTag("Player"))
+            {
+                // 리지드바디(Rigidbody)가 있는 오브젝트만 힘을 받음.
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    // 회오리 중심으로 향하는 방향 벡터
+                    Vector3 direction = transform.position - hit.transform.position;
+
+                    // 이 방향으로 힘을 적용
+                    rb.AddForce(direction.normalized * currentPullForce, ForceMode.Force);
+                }
+            }
+        }
+        
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow; // 기즈모 색상을 노란색으로 설정
+        Gizmos.DrawSphere(transform.position, pullRadius); // pullRadius 크기의 구를 그림
+    }
+}
