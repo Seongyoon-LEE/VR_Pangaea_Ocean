aaec45271490e8509cbd4bf8ffb0329a93081f45
soer1013 <kshhsk156@gmail.com>
2025-09-10 11:42:29 +0900

    feat: 스테이지 지형 보간 및 바다 수정
    위와 같습니다.

--- Assets/02.Scripts/KHB/GateTrigger.cs
+++ Assets/02.Scripts/KHB/GateTrigger.cs
@@ -5,17 +5,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class GateTrigger : MonoBehaviour
 {
-    // 유니티 에디터에서 쇠창살 에셋의 부모 오브젝트를 연결합니다.
     public GameObject gateObject;
 
-    // 소켓 인터랙터 컴포넌트를 자동으로 가져올 변수입니다.
     private XRSocketInteractor socketInteractor;
 
     void Start()
     {
         socketInteractor = GetComponent<XRSocketInteractor>();
 
-        // 소켓에 열쇠가 들어갔을 때(Select Entered) 호출될 함수를 연결합니다.
+        // 소켓에 열쇠가 들어갔을 때(Select Entered) 호출될 함수 연결
         if (socketInteractor != null)
         {
             socketInteractor.selectEntered.AddListener(OnKeyPlaced);
@@ -24,7 +22,7 @@ public class GateTrigger : MonoBehaviour
 
     private void OnKeyPlaced(SelectEnterEventArgs interactable)
     {
-        // 소켓에 들어온 오브젝트가 '열쇠' 태그를 가졌는지 확인합니다.
+        // 소켓에 들어온 오브젝트가 '열쇠' 태그를 가졌는지 확인
         if (interactable.interactableObject.transform.gameObject.CompareTag("Key"))
         {
             // 쇠창살 애니메이션 재생 로직
@@ -33,7 +31,7 @@ public class GateTrigger : MonoBehaviour
                 Animator animator = gateObject.GetComponent<Animator>();
                 if (animator != null)
                 {
-                    // "OpenGate" 트리거를 설정하여 애니메이션을 재생합니다.
+                    // "OpenGate" 트리거를 설정하여 애니메이션을 재생
                     animator.SetTrigger("OpenGate");
                 }
             }
