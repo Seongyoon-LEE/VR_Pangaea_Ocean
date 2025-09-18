eab97005a57153492b621b0f70ce3e5a928d2bd8
Seongyoon-LEE <ssyy9727@naver.com>
2025-09-18 17:29:11 +0900

    fix: 해초 이즈트리거 체크

--- Assets/02.Scripts/SCM/Player/PlayerPositionY.cs
+++ Assets/02.Scripts/SCM/Player/PlayerPositionY.cs
@@ -2,7 +2,6 @@ using ESSW.Editorcontroller;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerPositionY : MonoBehaviour
 {
