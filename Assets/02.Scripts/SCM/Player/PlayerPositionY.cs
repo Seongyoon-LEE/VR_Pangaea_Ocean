c7438402666ce5f9e49df3ec8c811987ee95c5dc
schang94 <changmin947@gmail.com>
2025-09-19 15:51:13 +0900

    feat: 수면 상태 변경 로직 변경

--- Assets/02.Scripts/SCM/Player/PlayerPositionY.cs
+++ Assets/02.Scripts/SCM/Player/PlayerPositionY.cs
@@ -25,15 +25,20 @@ public class PlayerPositionY : MonoBehaviour
 
     IEnumerator PlayerPosition()
     {
+        // 처음 시작 했을 때 위치에 대한 값으로 결정
         isEnter = !(transform.position.y > -1.3f);
         while (true)
         {
             yield return ws;
             
-            if (transform.position.y > -1.3f)
+            if (transform.position.y > -1.3f) // 물 밖 있을 때
             {
-                isEnter = false;
-                GameManager.Instance.state = GameManager.State.RECOVERY;
+                if (!isEnter) continue; // 물 밖에 계속 있었을 때 continue
+                isEnter = false; // 처음 물 밖에 나왔을 때 상태 변경
+                
+                GameManager.Instance.state = GameManager.State.RECOVERY; // 회복 상태로 변경
+                // 물 회전 값 변경
+                // 스테이지 변경시 물 오브젝트가 사라져 다시 찾아준다.
                 try
                 {
                     water.rotation = new Quaternion(0, 0, 0, 0);
@@ -44,14 +49,13 @@ public class PlayerPositionY : MonoBehaviour
                     water.rotation = new Quaternion(0, 0, 0, 0);
                 }
             }
-            else
+            else // 물 속에 있을 때
             {
-                water.rotation = new Quaternion(1, 0, 0, 0);
-                if (!isEnter)
-                {
-                    isEnter = true;
-                    GameManager.Instance.state = GameManager.State.NORMAL;
-                }
+                if (isEnter) continue; // 물 속에 있을 때 
+
+                isEnter = true; // 물 속에 있는 상태로
+                water.rotation = new Quaternion(1, 0, 0, 0); // 물 회전
+                GameManager.Instance.state = GameManager.State.NORMAL; // 일반 상태로 변경
             }
         }
     }
