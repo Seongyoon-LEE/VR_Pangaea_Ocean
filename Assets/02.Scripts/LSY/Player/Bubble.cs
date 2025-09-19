873ee3144ed19a7c07a7a88999f3ea39c30c6bac
Seongyoon-LEE <ssyy9727@naver.com>
2025-09-19 16:08:01 +0900

    feat: 물 들어가는 사운드 나가는 사운드 기능 구현, 주기적 거품 발생및 사운득 ㅣ능 구현

---
+++ Assets/02.Scripts/LSY/Player/Bubble.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [Header("스크립트 참조")]
+    [SerializeField] SwimMovement swimMovement;
+    [SerializeField] Transform bubbleSpawnPoint;
+
+    [SerializeField] AudioClip bubbleClip; // 거품 사운드
+
+    [SerializeField] float bubbleInterval = 10f; // 거품 생성 간격
+    void Start()
+    {
+        swimMovement = GetComponent<SwimMovement>();
+        bubbleSpawnPoint = transform.GetChild(2).GetComponent<Transform>();
+        // 10초마다 거품 생성 코루틴
+        StartCoroutine(SpawnBubblesRoutine());
+    }
+    IEnumerator SpawnBubblesRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(bubbleInterval);
+            SpawnBubble();
+        }
+    }
+    void SpawnBubble()
+    {
+        if (swimMovement != null && swimMovement.isUnderwaterNow)
+        {
+            SoundManager.s_Instance.PlaySfx(transform.position,bubbleClip, false);
+            // 이펙트 풀링에서 거품 이펙트 가져오기
+            var bubbleEffect = EffectPoolingManager.Instance.GetFromPool(
+                EffectPoolingManager.Instance.bubbleEffectPoolList,
+                EffectPoolingManager.Instance.bubbleEffectPrefab);
+
+            if(bubbleEffect != null)
+            {
+                bubbleEffect.transform.position = bubbleSpawnPoint.position;
+                bubbleEffect.SetActive(true);
+            }
+        }
+    }
+}
